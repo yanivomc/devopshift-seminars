2092bdfea6afddf49f9e96d128fe9ccdb51ff258
yanivomc-k1 <yanivomc@gmail.com>
2019-12-19 01:05:21 +0200

    skaffold microservices

---
+++ k8s/day_1/skaffold/microservices/devopshift-app/app.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Devopshift app!!\n")
+}
+
+func main() {
+	log.Print("Devopshift app server ready")
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":50051", nil)
+}
