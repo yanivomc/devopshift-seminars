5e81a0dd189ae6fb72efc613ae2d8b1e62f3b0f5
yanivomc <yanivomc@gmail.com>
2021-04-07 11:28:32 +0300

    revert file

--- docker/day_2/12factor/multistage_build/flask-api/api/_05_flask_restful_simple.py
+++ docker/day_2/12factor/multistage_build/flask-api/api/_05_flask_restful_simple.py
@@ -1,21 +1,12 @@
-### FIXED FILE
 from flask import Flask, abort, request
 from flask_restful import Resource, Api
 from .utils import search_book
-
-# Fixed GLOBAL with Class and resource (PYTHON 3 WAY)
-class Last_id(Resource):
-    def __init__(self, num):
-        self.num = num
-
-    def last_id(self):
-        return self.num
 
 
 app = Flask(__name__)
 api = Api(app)
 
-
+LAST_ID = 33
 books = [{
     'id': 33,
     'title': 'The Raven',
@@ -43,7 +34,7 @@ class BookListResource(Resource):
         return books
 
     def post(self):
-        LAST_ID = Last_id(33)
+        global LAST_ID
         LAST_ID += 1
         data = request.json
         book = {
